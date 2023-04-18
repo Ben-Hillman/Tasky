89d369afbf9bcbab5f829ae7308feb06833cdcb0
Ben Hillman <123774775+Ben-Hillman@users.noreply.github.com>
2023-04-18 15:49:58 -0400

    Added quality of life changes.
    Added local storage to store tasks, removing a task, removes it from storage.
    Made task content editable after the task is created.
    Used the pre tag to display the task description allowing for new lines to be created.

---
+++ App.js
@@ -0,0 +1,74 @@
+// import React, { useState } from "react";
+// import "./App.css";
+
+// import Sidebar from "./components/Sidebar";
+// import Task from "./components/Task";
+
+// function App() {
+//   const [tasks, setTasks] = useState([]);
+
+//   const addTask = (newTask) => {
+//     setTasks((prevTasks) => [...prevTasks, newTask]);
+//   };
+
+//   const removeTask = (taskIdToRemove) => {
+//     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskIdToRemove));
+//   };
+
+//   return (
+//     <div className="app-container">
+//       <Sidebar addTask={addTask} />
+//       <div className="task-feed">
+//         {tasks.map((task) => (
+//           <Task key={task.id} task={task} removeTask={removeTask} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import Sidebar from "./components/Sidebar";
+import Task from "./components/Task";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const removeTask = (taskIdToRemove) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskIdToRemove));
+    localStorage.removeItem("task-" + taskIdToRemove);
+  };
+
+  useEffect(() => {
+    const savedTasks = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key.startsWith("task-")) {
+        const task = JSON.parse(localStorage.getItem(key));
+        savedTasks.push(task);
+      }
+    }
+    setTasks(savedTasks.reverse());
+  }, []);
+
+  return (
+    <div className="app-container">
+      <Sidebar addTask={addTask} />
+      <div className="task-feed">
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} removeTask={removeTask} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ TaskForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "../styles/taskform.css";
+
+function TaskForm(props) {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name.length !== 0) {
+      const task = { 
+        id: Date.now(), 
+        name, 
+        date, 
+        description 
+      };
+      props.addTask(task);
+    }
+    setName("");
+    setDate("");
+    setDescription("");
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <label>Enter Task:</label>
+      <input
+        type="text"
+        name="task"
+        placeholder="ex: workout"
+        value={name}
+        onChange={handleNameChange}
+        required
+      />
+      <label>Enter Date:</label>
+      <input 
+        type="date" 
+        name="date" 
+        value={date} 
+        onChange={handleDateChange} 
+        required />
+      <label>Add Description:</label>
+      <textarea
+        name="description"
+        placeholder="ex: 1 hour workout"
+        value={description}
+        onChange={handleDescriptionChange}
+      />
+      <button className="add-task-btn" type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default TaskForm;
