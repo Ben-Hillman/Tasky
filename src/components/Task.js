a314f4933ce2bc361d2eb07e6a6fc1266232ff18
Ben Hillman <123774775+Ben-Hillman@users.noreply.github.com>
2023-04-18 15:59:57 -0400

    Add files via upload

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,42 @@
-import React from "react";
+// import React from "react";
+// import "../styles/task.css";
+
+// function Task(props) {
+//   const { task, removeTask } = props;
+
+//   const handleRemoveClick = () => {
+//     removeTask(task.id);
+//   };
+
+//   const formatDate = (date) => {
+//     const options = {
+//       year: "numeric",
+//       month: "long",
+//       day: "numeric",
+//       timeZone: "UTC"
+//     };
+//     return new Date(date).toLocaleDateString(undefined, options);
+//   };
+  
+  
+  
+
+//   return (
+//     <div className="task-container">
+//       <div className="task-header">
+//         <h2 contentEditable="true">{task.name}</h2>
+//         <i className="fa-solid fa-xmark" onClick={handleRemoveClick}></i>
+//       </div>
+//       <span className="task-date" contentEditable="true">{formatDate(task.date)}</span>
+//       <hr />
+//       <pre className="task-description" contentEditable="true">{task.description}</pre>
+//     </div>
+//   );
+// }
+
+// export default Task;
+
+import React, { useEffect } from "react";
 import "../styles/task.css";
 
 function Task(props) {
@@ -18,18 +56,20 @@ function Task(props) {
     return new Date(date).toLocaleDateString(undefined, options);
   };
   
-  
-  
+  // Save the task to local storage when it changes
+  useEffect(() => {
+    localStorage.setItem("task-" + task.id, JSON.stringify(task));
+  }, [task]);
 
   return (
     <div className="task-container">
       <div className="task-header">
-        <h2>{task.name}</h2>
+        <h2 contentEditable="true">{task.name}</h2>
         <i className="fa-solid fa-xmark" onClick={handleRemoveClick}></i>
       </div>
-      <span className="task-date">{formatDate(task.date)}</span>
+      <span className="task-date" contentEditable="true">{formatDate(task.date)}</span>
       <hr />
-      <p className="task-description">{task.description}</p>
+      <pre className="task-description" contentEditable="true">{task.description}</pre>
     </div>
   );
 }
