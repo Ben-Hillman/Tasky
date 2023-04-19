9238453463bc49f2377644a49341346fd07ee3ec
Ben Hillman <123774775+Ben-Hillman@users.noreply.github.com>
2023-04-19 14:02:14 -0400

    Add files via upload

--- src/components/Task.js
+++ src/components/Task.js
@@ -36,11 +36,20 @@
 
 // export default Task;
 
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import "../styles/task.css";
+
+// function Task(props) {
+//   const { task, removeTask } = props;
+
+import React, { useState } from "react";
 import "../styles/task.css";
 
 function Task(props) {
-  const { task, removeTask } = props;
+  const { task, removeTask, updateTask } = props;
+  const [name, setName] = useState(task.name);
+  const [date, setDate] = useState(task.date);
+  const [description, setDescription] = useState(task.description);
 
   const handleRemoveClick = () => {
     removeTask(task.id);
@@ -56,20 +65,42 @@ function Task(props) {
     return new Date(date).toLocaleDateString(undefined, options);
   };
   
-  // Save the task to local storage when it changes
-  useEffect(() => {
-    localStorage.setItem("task-" + task.id, JSON.stringify(task));
-  }, [task]);
+  // // Save the task to local storage when it changes
+  // useEffect(() => {
+  //   localStorage.setItem("task-" + task.id, JSON.stringify(task));
+  // }, [task]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.textContent);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.textContent);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.textContent);
+  };
+
+  const handleBlur = () => {
+    const updatedTask = {
+      id: task.id,
+      name,
+      date,
+      description,
+    };
+    updateTask(updatedTask);
+  };
 
   return (
     <div className="task-container">
       <div className="task-header">
-        <h2 contentEditable="true">{task.name}</h2>
+        <h2 contentEditable="true" onBlur={handleBlur} onInput={handleNameChange}>{task.name}</h2>
         <i className="fa-solid fa-xmark" onClick={handleRemoveClick}></i>
       </div>
-      <span className="task-date" contentEditable="true">{formatDate(task.date)}</span>
+      <span className="task-date" contentEditable="true" onBlur={handleBlur} onInput={handleDateChange}>{formatDate(task.date)}</span>
       <hr />
-      <pre className="task-description" contentEditable="true">{task.description}</pre>
+      <pre className="task-description" contentEditable="true" onBlur={handleBlur} onInput={handleDescriptionChange}>{task.description}</pre>
     </div>
   );
 }
